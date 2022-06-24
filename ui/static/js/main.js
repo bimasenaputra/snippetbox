49c99672ff3b7cfa6a14f82a460acbd667bc4a02
Bimasena Putra <bimasenaputra@gmail.com>
2022-06-24 20:54:49 +0700

    Add sticky and live navbar

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,8 +1,23 @@
-var navLinks = document.querySelectorAll("nav a");
-for (var i = 0; i < navLinks.length; i++) {
-	var link = navLinks[i]
-	if (link.getAttribute('href') == window.location.pathname) {
-		link.classList.add("live");
-		break;
+function init() {
+	var navbar = document.getElementsByTagName("nav")[0];
+	var navPos = navbar.offsetTop;
+	let navLinks = document.querySelectorAll("nav a");
+	
+	for (let i = 0; i < navLinks.length; i++) {
+		var link = navLinks[i]
+		if (link.getAttribute('href') == window.location.pathname) {
+			link.classList.add("live");
+			break;
+		}
 	}
+
+	window.addEventListener('scroll', () => {
+		if (window.scrollY >= navPos) {
+			navbar.classList.add("sticky");
+		} else {
+			navbar.classList.remove("sticky");
+		}
+	})
 }
+
+window.onload = init
